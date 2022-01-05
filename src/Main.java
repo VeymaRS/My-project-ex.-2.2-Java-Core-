a98358983660471e8d43b1dd834c7b1c01c47b1d
Anton <weymar87@gmail.com>
2022-01-05 12:18:37 +0500

    Added third action for streaming. Corrections made.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
         List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
         Collection<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 100; i++) {
             persons.add(new Person(
                     names.get(new Random().nextInt(names.size())),
                     families.get(new Random().nextInt(families.size())),
@@ -17,15 +17,39 @@ public class Main {
             );
         }
         long count = persons.stream()
-                .filter(x -> x.getAge() > 18)
+                .filter(x -> x.getAge() >= 18)
                 .count();
+
+        System.out.println("\nКоличество совершеннолетних\n");
         System.out.println(count);
 
-        List<String> listOfconscript = persons.stream()
-                .filter(x -> x.getAge() > 18)
-                .filter(x -> x.getAge() < 27)
+        final List<String> listOfConscript = persons.stream()
+                .filter(x -> x.getAge() >= 18)
+                .filter(x -> x.getAge() <= 27)
                 .map(b -> b.getFamily())
                 .collect(Collectors.toList());
-        for (String i : listOfconscript) System.out.println(i);
+
+        System.out.println("\nПофамильный список призывников\n");
+        for (String i : listOfConscript) System.out.println(i);
+
+        final List<Person> listOfWorkerMale = persons.stream()
+                .filter(x -> x.getAge() <= 65)
+                .filter(x -> x.getAge() >= 18)
+                .filter(x -> x.getEducation().equals(Education.HIGHER))
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+
+        final List<Person> listOfWorkerFemale = persons.stream()
+                .filter(x -> x.getSex().equals(Sex.FEMALE))
+                .filter(x -> x.getAge() <= 60)
+                .filter(x -> x.getAge() >= 18)
+                .filter(x -> x.getEducation().equals(Education.HIGHER))
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+
+        System.out.println("\nПофамильный список работоспособных мужчин\n");
+        for (Person i : listOfWorkerMale) System.out.println(i);
+        System.out.println("\nПофамильный список работоспособных женщин\n");
+        for (Person i : listOfWorkerFemale) System.out.println(i);
     }
 }
